3d9c7642652eefa6d50b9dceccd82342c0709c88
Subasangeeth <142552707+subasangeeth@users.noreply.github.com>
2025-10-20 16:50:03 +0530

    updated app.js
    Updated response message to include version 1.1.

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200);
-  res.end('Hello CICD PipeLine using GitHub Actions \n');
+  res.end('Hello CICD PipeLine using GitHub Actions and version 1.1\n');
 });
 
 server.listen(8081, () => {
   console.log('Server running at http://localhost:8081/');
 });
+
 
